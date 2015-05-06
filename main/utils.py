2e26cc35491fa7e9ac5d6f761e80abe7e15df765
Jordon Wing <jordonwii@gmail.com>
2015-05-05 23:30:31 -0700

    Replace simplejson with json

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,7 @@
-import simplejson
+import json
 from django.http import HttpResponse
 
 class JSONResponse(HttpResponse):
     def __init__(self, data, *args, **kwargs):
         kwargs['content_type'] = kwargs.get('content_type', 'application/json')
-        super(JSONResponse, self).__init__(simplejson.dumps(data), *args, **kwargs)
+        super(JSONResponse, self).__init__(json.dumps(data), *args, **kwargs)
