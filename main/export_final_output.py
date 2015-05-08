df1466a334fe7046fac53771b77ad8f8c0f0dcad
Jordon Wing <jordonwii@gmail.com>
2015-05-08 14:16:07 -0700

    Added export script

---
+++ main/export_final_output.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os, shutil, sys, argparse, time
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nptsp_leaders.settings'
+import django
+django.setup()
+
+from main.models import InputGraph
+
+def main(fname):
+    graphs = InputGraph.objects.exclude(is_test_graph=True).order_by("input_filename")
+    linenums = {}
+    for graph in graphs:
+        input_id = int(graph.input_filename.split(".")[0])
+        linenums[input_id] = graph
+
+    with open(fname, "w") as f:
+        for line in range(1, len(graphs)+1):
+            with linenums[line].current_best.output_file.open() as output:
+                f.write(output.read())
+                if line != len(graphs):
+                    f.write("\n")
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "Provide exactly one argument: python %s myoutputname.out" % __file__
+    else:
+        fname = sys.argv[1]
+        main(fname)
