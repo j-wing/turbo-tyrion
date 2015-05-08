9954f76d0d86099f1684d034d913ed1d246c4915
Jordon Wing <jordonwii@gmail.com>
2015-05-08 14:16:57 -0700

    Fixed eggregious typo in add_result.

--- main/views.py
+++ main/views.py
@@ -113,7 +113,7 @@ def add_result(request, graph_id):
             algo = Algorithm(name=post['algorithm_name'], command=post['algorithm_command'])
             algo.save()
     
-    score = GraphScore(algo=algo, graph=graph, path_cost=int(post['path_cost']), path=path_cost, runtime=post['runtime'])
+    score = GraphScore(algo=algo, graph=graph, path_cost=path_cost, path=post['path'], runtime=post['runtime'])
     score.output_file.save("", ContentFile(post['out']))
     score.save()
 
